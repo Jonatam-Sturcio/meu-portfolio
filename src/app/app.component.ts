0fed5f6fbb352b466470d152576c0d1dd2558ace
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-17 16:13:53 -0300

    Pagina Sobre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
+import { NgClass, NgFor, NgForOf, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [NgClass],
+  templateUrl: "app.component.html",
+  styleUrl: "app.component.scss"
 })
 export class AppComponent {
-  title = 'meu-portifolio';
+  public colapsarNavBar: boolean = false;
+
 }
