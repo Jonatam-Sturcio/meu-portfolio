1a3b9b405fafa0d87b5a5749552eee4d97cda002
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-23 14:18:20 -0300

    Ajuste no angular.sjon

--- src/app/components/secoes/stack/stack.component.ts
+++ src/app/components/secoes/stack/stack.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
-import { ItemStack } from './models/item-stack';
-import { NgForOf } from '@angular/common';
+import { Component } from "@angular/core";
+import { ItemStack } from "./models/item-stack";
+import { NgForOf } from "@angular/common";
 
 @Component({
-  selector: 'app-stack',
+  selector: "app-stack",
   standalone: true,
   imports: [NgForOf],
-  templateUrl: './stack.component.html',
-  styleUrl: './stack.component.scss'
+  templateUrl: "./stack.component.html",
+  styleUrl: "./stack.component.scss",
 })
 export class StackComponent {
-public itensStack: ItemStack[] = [
-  {titulo: ".NET"},
-  { titulo: "Angular" },
-  { titulo: "TypeScript" },
-  { titulo: "Node.js" },
-  { titulo: "Firebase" },
-  { titulo: "Entity Framework" },
-  { titulo: "Microsoft SQL Server" },
-  { titulo: "Selenium" },
-]
+  public itensStack: ItemStack[] = [
+    { titulo: ".NET" },
+    { titulo: "Angular" },
+    { titulo: "TypeScript" },
+    { titulo: "Node.js" },
+    { titulo: "Firebase" },
+    { titulo: "Entity Framework" },
+    { titulo: "Microsoft SQL Server" },
+    { titulo: "Selenium" },
+    { titulo: "Flutter" },
+    { titulo: "Java" },
+    { titulo: "ThingsBoard" },
+  ];
 }
