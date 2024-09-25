9c51d8dfbd95235330665c1037cc186705b9cde8
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-25 16:47:39 -0300

    Atualização Portfolio

--- src/app/components/secoes/portfolio/portfolio.component.ts
+++ src/app/components/secoes/portfolio/portfolio.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { NgForOf } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-portfolio',
+  selector: "app-portfolio",
   standalone: true,
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  imports: [NgForOf],
+  templateUrl: "./portfolio.component.html",
+  styleUrl: "./portfolio.component.scss",
 })
 export class PortfolioComponent {
+  carrossel: Carrossel[] = [
+    {
+      nome: "Locadora",
+      img: "locadora.png",
+      link: "https://github.com/Jonatam-Sturcio/LocadoraDeVeiculosV2",
+    },
+    {
+      nome: "Pokedex",
+      img: "pokedex.png",
+      link: "https://jonatam-sturcio.github.io/pokedex/",
+    },
+  ];
+}
 
+interface Carrossel {
+  nome: string;
+  img: string;
+  link: string;
 }
